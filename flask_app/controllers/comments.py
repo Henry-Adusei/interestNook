3ed9a584490c9a8ce4325e0e5956b8d939b7962e
NgChris415 <132420552+NgChris415@users.noreply.github.com>
2023-08-22 22:33:48 -0700

    Finalized routes for adding comments, edited templates with appropriate routes

--- flask_app/controllers/comments.py
+++ flask_app/controllers/comments.py
@@ -3,24 +3,19 @@ from flask import render_template, redirect, request, session
 from flask_app.models.comment import Comments
 from flask_app.models.post import Post
 
-@app.route("/comment/<int:post_id>")
-def comment_form(post_id):
-    if 'user_id' not in session:
-        return redirect('/clear')
-    return render_template('comment.html', post = Post.get_one({'id':post_id}))
-
-@app.route("/comment/<int:post_id>/submit")
+@app.route("/comment/<int:post_id>/submit", methods = ['POST'])
 def submit_comment(post_id):
     if 'user_id' not in session:
         return redirect('/clear')
     if not Comments.validate_comment(request.form):
         return redirect(f'/comment/{post_id}')
     data = {
-        'post_id': post_id,
         'user_id': session['user_id'],
+        'post_id': post_id,
         'content' : request.form['content'],
     }
     Comments.add_comment(data)
+    print('MADE IT HERE####################################')
     return redirect('/dash')
 
 @app.route("/comment/delete/<int:comment_id>")
