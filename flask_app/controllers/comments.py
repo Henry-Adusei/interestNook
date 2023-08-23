fe494f8e686212c5121bb62dcc30af1b4b3b7610
NgChris415 <132420552+NgChris415@users.noreply.github.com>
2023-08-22 22:05:58 -0700

    Updated routes to include delete route

--- flask_app/controllers/comments.py
+++ flask_app/controllers/comments.py
@@ -6,23 +6,30 @@ from flask_app.models.post import Post
 @app.route("/comment/<int:post_id>")
 def comment_form(post_id):
     if 'user_id' not in session:
-        return redirect('/logout')
-    data = {
-        'id': session['user_id']
-    }
+        return redirect('/clear')
     return render_template('comment.html', post = Post.get_one({'id':post_id}))
 
 @app.route("/comment/<int:post_id>/submit")
 def submit_comment(post_id):
     if 'user_id' not in session:
-        return redirect('/logout')
-    data = {
-        'id': session['user_id']
-    }
+        return redirect('/clear')
+    if not Comments.validate_comment(request.form):
+        return redirect(f'/comment/{post_id}')
     data = {
         'post_id': post_id,
         'user_id': session['user_id'],
         'content' : request.form['content'],
     }
     Comments.add_comment(data)
-    return redirect('/dash', post = Post.get_one({'id':post_id}))
+    return redirect('/dash')
+
+@app.route("/comment/delete/<int:comment_id>")
+def delete_comment(comment_id):
+    if 'user_id' not in session:
+        return redirect('/clear')
+    data = {
+        'id' : comment_id
+    }
+    Comments.delete_comment(data)
+    return redirect('/dash', comment = Comments.get_comment({'id':comment_id}))
+
